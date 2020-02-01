15d64df65bb29e98f46d8270424fbb15866af29f
derek <dereknelson01@gmail.com>
2020-02-01 18:29:52 -0500

    Updating gamestates.

---
+++ src/event-types.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ChangeActiveDrug: 'change-active-drug',
+  ChangeActiveHammer: 'change-active-drug',
+  ChangeDoglars: 'change-doglars',
+  ChangeDrugs: 'change-drugs',
+  ChangeHammers: 'change-hammers'
+}
--- src/game-state.js
+++ src/game-state.js
@@ -1,33 +1,132 @@
+const EventTypes = require('./event-types');
+
 class GameState {
-
   static load() {
-    return {
-      health: 100,
-      doglars:1000,
-      activeDrug: 'skeeze',
-      activeHammer: 'rock',
-      drugs: {
-        skeeze: 0,
-        chlorine_triflouride: 0,
-        research: 0,
-        canadian_frog: 0
-      },
-      hammers: {
-        rock: 0,
-        brick: 0,
-        steel: 0,
-        gold: 0,
-      },
-      affects: {
-
-      }
+    if(!GameState.state) {
+      GameState.state = Object.assign({}, initialState);
     }
   }
 
-  static save() {
-    throw 'fit';
+  static reset() {
+    GameState.state = null;
+    return load();
   }
 
+  static save(obj) {
+    GameState.state.current = obj;
+  }
+
+  constructor(obj) {
+    this.events = new Phaser.Events.EventEmitter();
+    this.current = obj;
+  }
+
+  adjustHealth(amount) {
+    let currentHealth = this.current.playerState.health,
+      newHealth = currentHealth = Math.min(currentHealth + amount, this.current.gameState.player.healthMax);
+
+    this.events.emit({
+      type: EventTypes.ChangeHealth,
+      amount: amount,
+      health: currentHealth
+    })
+  }
+
+  adjustHammers(type, amount) {
+    this.events.emit({
+      type: EventTypes.ChangeHammers,
+    })
+  }
+
+  adjustDoglars(amount) {
+    this.events.emit({
+      type: EventTypes.ChangeDoglars,
+    })
+  }
+
+  useDrug(amount) {
+    let newValue = this.current.playerState.drugs[this.current.playerState.activeDrug] -= amount;
+
+    this.events.emit({
+      type: EventTypes.ChangeDrugs,
+      drug: this.current.playerState.activeDrug,
+      delta: amount,
+      value: newValue
+
+    })
+  }
+
+  set activeDrug(type) {
+    this.current.playerState.activeDrug = type;
+
+    this.events.emit({
+      type: EventTypes.ChangeActiveDrug,
+      drug: type
+    });
+  }
+
+  set activeHammer(type) {
+    this.current.playerState.activeHammer = type;
+    this.events.emit({
+      type: EventTypes.ChangeActiveHammer,
+      hammer: type
+    });
+  }
 }
+
+const initialState = {
+  playerState: {
+    health: 100,
+    doglars:50,
+    activeDrug: null,
+    activeHammer: 'rock',
+    drugs: {
+      skeeze: 0,
+      chlorine_triflouride: 0,
+      research: 0,
+      canadian_frog: 0
+    },
+    hammers: {
+      rock: 10,
+      brick: 0,
+      steel: 0,
+      gold: 0,
+    },
+    effects: {
+
+    }
+  },
+  gameState: {
+    player: {
+      healthMax: 150
+    },
+    hammers: {
+      rock: {
+        name: "rock",
+        cost: 10,
+        amount: 100,
+        repairValue: 1
+      },
+      brick: {
+        name: "brick",
+        cost: 0,
+        amount: 100,
+        repairValue: 5
+      },
+      steel: {
+        name: "steel",
+        cost: 0,
+        amount: 100,
+        repairValue: 100
+      },
+      gold: {
+        name: "gold",
+        cost: 0,
+        amount: 100,
+        repairValue: 1000
+      },
+    },
+  }
+};
 
 module.exports = GameState;
