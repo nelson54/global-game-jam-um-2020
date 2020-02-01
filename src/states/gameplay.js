bd0a5b8093c98aa08016ec1c6d03005413b6a073
derek <dereknelson01@gmail.com>
2020-02-01 16:07:12 -0500

    Moving around sparkles

--- src/states/gameplay.js
+++ src/states/gameplay.js
@@ -7,12 +7,14 @@ const Hud = require('../hud/hud');
 
 class Gameplay extends Phaser.State {
   preload() {
+    this.game.load.image('transparent', '/assets/nil.png');
 
     this.game.load.image('hammer-1', '/assets/sprites/regular_hammer.png');
 
     this.game.load.image('sparkle-1', '/assets/effects/yellow1.png');
     this.game.load.image('sparkle-2', '/assets/effects/yellow2.png');
     this.game.load.image('sparkle-3', '/assets/effects/yellow3.png');
+
     this.game.load.image('white-sparkle-1', '/assets/effects/white1.png');
     this.game.load.image('white-sparkle-2', '/assets/effects/white2.png');
     this.game.load.image('white-sparkle-3', '/assets/effects/white3.png');
